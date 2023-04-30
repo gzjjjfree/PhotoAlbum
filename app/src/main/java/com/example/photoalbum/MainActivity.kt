48dd75c6e2b8571d9c8fd0816beccce868634fea
gzjjjfree <gzjjj1234@gmail.com>
2023-04-30 17:19:21 +0800

    Initial commit

--- app/src/main/java/com/example/photoalbum/MainActivity.kt
+++ app/src/main/java/com/example/photoalbum/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -14,15 +15,22 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -47,31 +55,74 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun PhotoAlbumInterface(modifier: Modifier = Modifier) {
+    var result by remember { mutableStateOf(1) }
+    var photo_explian = R.string.photo_explian1
+    var imageResource = R.drawable.gz1
+    when (result) {
+        1 -> { imageResource = R.drawable.gz1
+            photo_explian = R.string.photo_explian1 }
+        2 -> { imageResource = R.drawable.gz2
+            photo_explian = R.string.photo_explian2 }
+        3 -> { imageResource = R.drawable.gz3
+            photo_explian = R.string.photo_explian3 }
+        4 -> { imageResource = R.drawable.gz4
+            photo_explian = R.string.photo_explian4 }
+        5 -> { imageResource = R.drawable.gz5
+            photo_explian = R.string.photo_explian5 }
+        6 -> { imageResource = R.drawable.gz6
+            photo_explian = R.string.photo_explian6 }
+        7 -> { imageResource = R.drawable.gz7
+            photo_explian = R.string.photo_explian7 }
+        8 -> { imageResource = R.drawable.gz8
+            photo_explian = R.string.photo_explian8 }
+        9 -> { imageResource = R.drawable.gz9
+            photo_explian = R.string.photo_explian9 }
+        else -> { imageResource = R.drawable.gz10
+            photo_explian = R.string.photo_explian10 }
+    }
     Column(modifier = Modifier.fillMaxHeight()
         .fillMaxWidth()
-        .padding(20.dp),
+        //.padding(20.dp)
+        .background(color = Color(0XFF99CCFF))
         //horizontalAlignment = Alignment.CenterHorizontally,
         //verticalArrangement = Arrangement.Center
     )
     {  //单列，下面三个组合项
-
-            Image(painter = painterResource(R.drawable.gz01),
-                contentDescription = null,
-                modifier = Modifier.weight(6F)
-                    .fillMaxWidth())
-
+Box(modifier = Modifier.weight(6F)
+    .fillMaxWidth()
+    .padding(20.dp)){
+    Image(painter = painterResource(imageResource),
+        contentDescription = null,
+        modifier = Modifier.fillMaxSize(),
+        //contentScale = ContentScale.Inside
+            //
+            )
+}
         Spacer(Modifier.height(24.dp))
-        Text(text = stringResource(R.string.photo_explian),
+        Text(text = stringResource(photo_explian),
             modifier = Modifier.weight(2F) //这里不选宽度是让整个文字居中，不然文字块全宽了还要设置里面
-            .align(Alignment.CenterHorizontally), //水平居中
-                fontSize = 30.sp,
+            .align(Alignment.CenterHorizontally) //水平居中
+            .padding(top = 50.dp),
+
+            textAlign = TextAlign.Center, //字体居中
+            fontWeight = FontWeight.Bold, //粗体
+                fontSize = 30.sp
            // modifier = Modifier.align(Alignment.CenterHorizontally)
         )
-
-        Spacer(Modifier.height(24.dp))
+       Spacer(Modifier.height(24.dp))
        Row(modifier = Modifier.weight(1F)
-           .fillMaxWidth()){
-
+           .align(Alignment.CenterHorizontally)
+           //.align(Alignment.Center)
+       ){
+           Button(onClick =
+           {
+               val x = result
+               while (x == result) {
+                   result = (1..10).random()
+               }
+           }) {
+               Text(text = "下一张")
+           }
        }
     }
 }
